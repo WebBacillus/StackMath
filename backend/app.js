5bb6664bb8eed00d5cddcfcf4c749d17cc8510d0
Pasit Khumsena <webbalaka@gmail.com>
2025-05-21 11:10:25 +0700

    fix api path

--- backend/app.js
+++ backend/app.js
@@ -17,60 +17,55 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   });
   
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 app.set("view engine", "html");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/easy", async (req, res) => {
+router.get("/easy", async (req, res) => {
   const items = await Item.aggregate([
     { $match: { diff : "easy"} },
     { $sample: { size: 1 } }
   ]);
-  // console.log(items);
-  // const items = await Item.find().sort({"Score" : -1}).limit(10);
   res.status(200).json(items);
 });
 
-app.get("/medium", async (req, res) => {
+router.get("/medium", async (req, res) => {
   const items = await Item.aggregate([
     { $match: { diff : "medium"} },
     { $sample: { size: 1 } }
   ]);
-  // const items = await Item.find().sort({"Score" : -1}).limit(10);
   res.status(200).json(items);
 });
 
-app.get("/hard", async (req, res) => {
+router.get("/hard", async (req, res) => {
   const items = await Item.aggregate([
     { $match: { diff : "hard"} },
     { $sample: { size: 1 } }
   ]);
-  // const items = await Item.find().sort({"Score" : -1}).limit(10);
   res.status(200).json(items);
 });
 
-app.get("/rank", async(req, res) => {
+router.get("/rank", async(req, res) => {
   const rank = await Rank.find().sort({"score" : -1})
-  // console.log(rank);
   res.status(200).json(rank);
 });
 
-app.post("/rank", async(req, res) => {
+router.post("/rank", async(req, res) => {
   try {
     const newRank = new Rank(req.body);
     console.log(req.body);
 
-    const items = await Rank.find({student_id : req.body.student_id}).maxTimeMS(1000 * 1000 * 1000);;
+    const items = await Rank.find({student_id : req.body.student_id}).maxTimeMS(1000 * 1000 * 1000);
     console.log(items);
     console.log(newRank);
     if(items.length == 0){
         console.log('save');
         await newRank.save();
     }
-    if(items[0].student_id == newRank.student_id) {
+    if(items[0]?.student_id == newRank.student_id) {
         console.log("matched");
         if(req.body.score > items[0].score){
             console.log('update');
@@ -88,22 +83,6 @@ app.post("/rank", async(req, res) => {
   }
 });
 
-//TODO ทำ req ranking
-
-// app.post("/rank_update", async(req, res) => {
-//     try {
-//       const newItem = new Item(req.body);
-//       console.log(newItem);
-//       await newItem.save();
-
-//       res.status(200).json({ message: "OK" });
-//     } catch (err) {
-//       if (err.name === "ValidationError") {
-//         res.status(400).json({ error: "Bad Request" });
-//       } else {
-//         res.status(500).json({ error: "Internal server error." });
-//       }
-//     }
-// })
+app.use("/api", router);
 
 export default app;
